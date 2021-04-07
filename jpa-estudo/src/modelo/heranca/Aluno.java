13f0ed83250c028d18f253a2f55c190f9b904f07
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-04-06 23:33:45 -0300

    Herança @Inheritance strategy JOINED
    Acredito que esta é a melhor solução pois é gerada uma tabela para cada classe separando assim as informações que são iguais para todas as classes como a classe Aluno, gerando apenas nome e matrícula, enquanto a Classe AlunoBolsista, foi gerado apenas as colunas amais como valordabolsa e é claro a coluna que contém o id que neste caso é matrícula.

--- jpa-estudo/src/modelo/heranca/Aluno.java
+++ jpa-estudo/src/modelo/heranca/Aluno.java
@@ -9,7 +9,7 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name = "tipo", length = 2, discriminatorType = DiscriminatorType.STRING)
 @DiscriminatorValue("AL")
 public class Aluno {
