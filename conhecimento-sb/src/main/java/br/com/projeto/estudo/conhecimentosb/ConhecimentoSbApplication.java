dff4f6663959f0e3af9f7de97fed7670a4dce58d
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-04-11 16:07:28 -0300

    Criando Primeiro Web Service Com Spring Boot

---
+++ conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/ConhecimentoSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.estudo.conhecimentosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConhecimentoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConhecimentoSbApplication.class, args);
+	}
+
+}
