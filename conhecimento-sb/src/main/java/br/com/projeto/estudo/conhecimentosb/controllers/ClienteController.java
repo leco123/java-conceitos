05dfe57ca0da661a7245ad67de1a0bfd14531b13
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-04-11 17:53:38 -0300

    Update ClienteController.java

--- conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/ClienteController.java
+++ conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/ClienteController.java
@@ -1,7 +1,5 @@
 package br.com.projeto.estudo.conhecimentosb.controllers;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
