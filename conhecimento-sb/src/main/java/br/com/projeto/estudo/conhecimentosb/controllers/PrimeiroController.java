20389627a0a528ccfd383318dbfef3a899f0c7ab
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-04-11 16:30:01 -0300

    Simulando Erros

--- conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/PrimeiroController.java
+++ conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/PrimeiroController.java
@@ -1,5 +1,6 @@
 package br.com.projeto.estudo.conhecimentosb.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,24 @@ public class PrimeiroController {
 	 * Define quais urls que seram chamadas quando carregar este método
 	 * @return
 	 */
-	@RequestMapping(path = {"/ola","/home"})
+	@RequestMapping(path = {"/ola","/saudacao"})
 	public String ola() {
+		return "Olá Spring Boot!";
+	}
+	
+	/**
+	 * Usando mesmo path, retorna erro mesmo sendo método diferente
+	 * @return
+	 */
+	/*
+	@RequestMapping(path = {"/saudacao"})
+	public String saudacao() {
+		return "Olá Spring Boot!";
+	}
+	*/
+	
+	@PostMapping(path = {"/saudacao"})
+	public String saudacao() {
 		return "Olá Spring Boot!";
 	}
 
