9c40a05ea64557e1e7cc3de1c06fe2fbc0d040e5
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-04-13 23:47:50 -0300

    Update ProdutoController.java

--- conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/ProdutoController.java
+++ conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/ProdutoController.java
@@ -14,6 +14,7 @@ import br.com.projeto.estudo.conhecimentosb.model.repositories.ProdutoRepository
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 	
+	// injeção de dependência
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
