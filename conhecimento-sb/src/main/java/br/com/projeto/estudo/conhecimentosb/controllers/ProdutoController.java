ce0f9976e4c4b0bf5e0e5bae7b223520fb6c982d
Alex de Carvalho <leco_123carvalho@hotmail.com>
2021-04-15 22:55:43 -0300

    Consulta Paginada

--- conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/ProdutoController.java
+++ conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/controllers/ProdutoController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +36,20 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}/qtdePagina/{qtdePagina}")
+	public Iterable<Produto> obterProduoPorPagina(
+			@PathVariable int numeroPagina, 
+			@PathVariable int qtdePagina) {
+		
+		// quantidade de elementos por página
+		if (qtdePagina <= 5 && qtdePagina < 10 ) qtdePagina = 3;
+		if (qtdePagina >= 10 && qtdePagina < 15 ) qtdePagina = 5;	
+		if (qtdePagina >= 15) qtdePagina = 8;
+		
+		Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+		return produtoRepository.findAll(page);
 	}
 	
 	@GetMapping(path = "/{id}")
--- conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/model/repositories/ProdutoRepository.java
+++ conhecimento-sb/src/main/java/br/com/projeto/estudo/conhecimentosb/model/repositories/ProdutoRepository.java
@@ -1,9 +1,9 @@
 package br.com.projeto.estudo.conhecimentosb.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.projeto.estudo.conhecimentosb.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
 }
